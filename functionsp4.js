a21c28974346770674a0319e5ea9c92ad7a3e9d6
unknown <daveasmah@yahoo.com>
2022-06-16 20:54:17 -0400

    final commit

--- functionsp4.js
+++ functionsp4.js
@@ -24,3 +24,21 @@ function multiplesOfNum() {
 
 // Callback function
 multiplesOfNum();
+
+
+//Solution
+function multiplesOfNum() {
+	for (let i = 1; i <= 100; i++){
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log("FizzBuzz");
+		} else if (i % 3 === 0) {
+			console.log("Fizz");
+		} else if (i % 5 === 0) {
+			console.log("Buzz");
+		} else {
+			console.log(i + " is not a multiple of 3, 5 or 15.");
+		}
+	}
+}
+
+multiplesOfNum();
